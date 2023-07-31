5844f2ddcf968b8480e409b40b0be1b5221f4c20
Stay <127594467+MioraNomenjanahary@users.noreply.github.com>
2023-07-31 20:15:24 +0300

    feat: add file via update

---
+++ index.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+
+export default function TodoList() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  // Fonction pour ajouter un nouvel élément à la liste
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodoList([...todoList, newTodo]);
+      setNewTodo('');
+    }
+  };
+
+  // Fonction pour gérer la saisie de texte dans la barre d'ajout
+  const handleInputChange = (e) => {
+    setNewTodo(e.target.value);
+  };
+
+  // Fonction pour ajouter un nouvel élément lorsque la touche "Entrée" est enfoncée
+  const handleEnterKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      addTodo();
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={handleInputChange}
+        onKeyPress={handleEnterKeyPress}
+      />
+      <button onClick={addTodo}>Ajouter</button>
+      <ul>
+        {todoList.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>Supprimer</button>
+            <button
+              onClick={() => {
+                const updatedText = prompt('Modifier le contenu :', todo);
+                if (updatedText !== null) {
+                  updateTodo(index, updatedText);
+                }
+              }}
+            >
+              Mettre à jour
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
